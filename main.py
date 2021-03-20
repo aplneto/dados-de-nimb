6b8ba3c5a5a06094144e13391f250bbe9789ed29
Paulino <paulinolimakl@gmail.com>
2021-03-20 08:59:23 +0000

    Rolagem funcional

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+
+'''
+
 import os
 from discord.ext import commands
 
@@ -5,6 +9,7 @@ import dados
 from keep_alive import keep_alive
 
 bot = commands.Bot(command_prefix="!")
+d = dados.Dados()
 
 @bot.event
 async def on_ready():
@@ -12,9 +17,15 @@ async def on_ready():
 
 @bot.command(name='rolar', help='Simula rolagem de dados')
 async def roll(ctx, string_dados):
-    r, m, t = dados.decifrar_rolagem(string_dados)
-    rolagem = '+'.join([str(x) for x in r+m])
-    await ctx.send(f"{rolagem}={t}")
+    t, r, mods = dados.Dados.rolar(string_dados)
+    rolagem = '+'.join([str(x) for x in r+mods])
+    rolagem = rolagem.replace("++", "+")
+    rolagem = rolagem.replace("+-", "-")
+    await ctx.send(f":game_die:: {rolagem} = {t}")
+
+@bot.command(name='teste', help='Simula um teste')
+async def test(ctx, string_dados):
+  pass
 
 # keep_alive();
 token = os.getenv("TOKEN")
